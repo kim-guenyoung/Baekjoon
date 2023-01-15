73ed062e04240408e43d0bb479a26497f65fec91
김근영 <rmsdud0527@naver.com>
2023-01-15 13:25:54 +0900

    [Bronze V] Title: 아스키 코드, Time: 216 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      int ch = scanner.next().charAt(0); //특정문자를 char 타입으로 변환하기 위해 사용함
+      System.out.println(ch);
+    }
+}
